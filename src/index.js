2f59dfe7d2bd0795cb93f24cec9cb49a4dfe5d6c
chrissanborn82 <Kurisanborn@gmail.com>
2024-05-27 21:13:09 -0400

    build a board

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import Phaser from 'phaser';
 import io from 'socket.io-client';
 import { v4 } from 'uuid';
+import correctImage from './image.json'
 
 console.log('Phaser:', Phaser);
 console.log('Socket.IO:', io);
-console.log('uiud', v4);
+console.log({ correctImage });
 
 // Import assets
 import sky from '../assets/sky.png';
@@ -12,6 +13,9 @@ import platform from '../assets/platform.png';
 import star from '../assets/star.png';
 import bomb from '../assets/bomb.png';
 import dude from '../assets/dude.png';
+import tiles from '../assets/tilemap.png';
+
+
 
 var config = {
     type: Phaser.AUTO,
@@ -51,6 +55,7 @@ function preload() {
     this.load.image('star', star);
     this.load.image('bomb', bomb);
     this.load.spritesheet('dude', dude, { frameWidth: 32, frameHeight: 48 });
+    this.load.spritesheet('tiles', tiles, { frameWidth: 15, frameHeight: 15 });
 }
 
 function create() {
@@ -82,106 +87,25 @@ function create() {
         }
     });
 
+    const map = this.make.tilemap({ width: 10, height: 10, tileWidth: 15, tileHeight: 15 });
+    const tileset = map.addTilesetImage('tiles');
+    const layer = map.createBlankLayer('layer1', tileset, 0, 0);
+    console.log(correctImage.board);
+    for (let y = 0; y < correctImage.board.length; y++) {
+        for (let x = 0; x < correctImage.board[y].length; x++) {
+            const tileIndex = correctImage.board[y][x] ? 2 : 1; // Assuming 0 is the first image, 1 is the second
+            console.log({ tileIndex, x, y })
+            layer.putTileAt(tileIndex, x, y);
+        }
+    }
 
     socket.emit('UserJoined', { uuid, x: 100, y: 450 });
     console.log(uuid);
-    //  A simple background for our game
-    this.add.image(400, 300, 'sky');
-
-    //  The platforms group contains the ground and the 2 ledges we can jump on
-    platforms = this.physics.add.staticGroup();
-
-    //  Here we create the ground.
-    //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
-    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
-
-    //  Now let's create some ledges
-    platforms.create(600, 400, 'ground');
-    platforms.create(50, 250, 'ground');
-    platforms.create(750, 220, 'ground');
-
-    players[uuid] = this.physics.add.sprite(100, 450, 'dude');
-    //  Player physics properties. Give the little guy a slight bounce.
-    players[uuid].setBounce(0.2);
-    players[uuid].setCollideWorldBounds(true);
-
-    //  Our player animations, turning, walking left and walking right.
-    this.anims.create({
-        key: 'left',
-        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
-        frameRate: 10,
-        repeat: -1
-    });
-
-    this.anims.create({
-        key: 'turn',
-        frames: [{ key: 'dude', frame: 4 }],
-        frameRate: 20
-    });
-
-    this.anims.create({
-        key: 'right',
-        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
-        frameRate: 10,
-        repeat: -1
-    });
-
-    //  Input Events
-    cursors = this.input.keyboard.createCursorKeys();
-
-    //  Some stars to collect, 12 in total, evenly spaced 70 pixels apart along the x axis
-    stars = this.physics.add.group({
-        key: 'star',
-        repeat: 11,
-        setXY: { x: 12, y: 0, stepX: 70 }
-    });
-
-    stars.children.iterate(function (child) {
-
-        //  Give each star a slightly different bounce
-        child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
-
-    });
-
-    bombs = this.physics.add.group();
-
-    //  The score
-    scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
-
-    //  Collide the player and the stars with the platforms
-
-    this.physics.add.collider(players[uuid], platforms);
-
-    this.physics.add.collider(stars, platforms);
-    this.physics.add.collider(bombs, platforms);
-
-    //  Checks to see if the player overlaps with any of the stars, if he does call the collectStar function
 }
 
 function update() {
     if (gameOver) {
         return;
-    }
-
-    if (cursors.left.isDown) {
-        // Send a test message to the server
-        players[uuid].setVelocityX(-160);
-
-        players[uuid].anims.play('left', true);
-    }
-    else if (cursors.right.isDown) {
-        players[uuid].setVelocityX(160);
-
-        players[uuid].anims.play('right', true);
-    }
-    else {
-        players[uuid].setVelocityX(0);
-
-        players[uuid].anims.play('turn');
-    }
-
-    if (cursors.up.isDown && players[uuid].body.touching.down) {
-        players[uuid].setVelocityY(-330);
     }
 }
 
---
+++ test.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+var PNGReader = require('png.js');
+
+fs.readFile('assets/fish.png', function (err, buffer) {
+
+    var reader = new PNGReader(buffer);
+    reader.parse(function (err, png) {
+        if (err) throw err;
+        console.log(png.getPixel(1, 0));
+        let map = new Array(10).fill().map(() => new Array(10).fill(0));
+        for (let i = 0; i < 10; i++) {
+            for (let j = 0; j < 10; j++) {
+                console.log(i, j)
+                map[i][j] = png.getPixel(i, j)[3] === 255
+            }
+        }
+        console.log(map);
+    });
+
+});
